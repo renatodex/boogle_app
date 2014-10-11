6f5696d89c87cb5ff544ffee43f5d254a6a02a75
renatodex <renatodex@gmail.com>
2014-10-11 18:57:08 -0300

    Added tests to build Boogle::API

--- spec/lib/boogle_spec.rb
+++ spec/lib/boogle_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'pry'
+require 'json'
 
 # MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_URI']}}, 'production')
 # MongoMapper.database = ENV['MONGODB_DB'] || "boogle"
@@ -40,7 +41,83 @@ class ScoreMachine
   end
 end
 
+class SomeClass
+  def self.method_to_retrieve_data(q)
+  end
+end
+
+module Boogle
+  class API
+    class << self
+      def search(query)
+        raw_result = ::SomeClass.method_to_retrieve_data(query)
+
+        matches = build_matches(query, raw_result)
+        sorted_matches = matches.sort {|p, n| n["score"] <=> p["score"]  }
+
+        { "matches" => sorted_matches }.to_json
+      end
+
+      def build_matches(query, raw_result)
+        result = raw_result.inject([]) do |arr, p|
+          score = ScoreMachine.calculate(p[:content], query)
+          if score > 0
+            arr << result_json(p[:pageId],score)
+          end
+          arr
+        end
+      end
+
+      def result_json(pageId, score)
+        { "pageId" => pageId, "score" => score}
+      end
+    end
+  end
+end
+
 describe "Boogle" do
+
+  let(:pages) {[
+    { :pageId => 300, :content => "the quick brown fox jumped over the lazy dog" }, #2
+    { :pageId => 301, :content => "brown is the fox, red is the chicken"}, #3
+    { :pageId => 302, :content => "Set was the operationg king, he has single purposes" }, #1
+    { :pageId => 303, :content => "I wanted him to return, due to everything he had given to me"}, #0
+  ]}
+
+  context "API Handling" do
+    it "should return score json given a set pages and a single query" do
+      query = "the fox is a very weaken mind man"
+      allow(SomeClass).to receive(:method_to_retrieve_data).and_return(pages)
+
+      expect(Boogle::API.search(query)).to eq({"matches" => [
+          {"pageId" => 301, "score" => 3},
+          {"pageId" => 300, "score" => 2},
+          {"pageId" => 302, "score" => 1}
+        ]
+      }.to_json)
+    end
+
+    it "should return score json given a set pages and a single query" do
+      query = "brown fox jumped"
+
+      allow(SomeClass).to receive(:method_to_retrieve_data).and_return(pages)
+      expect(Boogle::API.search(query)).to eq({"matches" => [
+          {"pageId" => 300, "score" => 3},
+          {"pageId" => 301, "score" => 2}
+        ]
+      }.to_json)
+    end
+
+    it "should return no matches when query does not match anything" do
+      query = "golden dolphins"
+
+      allow(SomeClass).to receive(:method_to_retrieve_data).and_return(pages)
+      expect(Boogle::API.search(query)).to eq({
+        "matches" => []
+      }.to_json)
+    end
+  end
+
   context "Score" do
     it "should score page search" do
       page_content = "the quick brown fox jumped over the lazy dog"
@@ -84,6 +161,8 @@ describe "Boogle" do
     end
   end
 end
+
+
 
 # context "API" do
 #   context "POST /index"
