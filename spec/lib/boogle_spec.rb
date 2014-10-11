301efc775048ea123fb0c922652122141603eefa
renatodex <renatodex@gmail.com>
2014-10-11 16:53:38 -0300

    Added first Score test

--- spec/lib/boogle_spec.rb
+++ spec/lib/boogle_spec.rb
@@ -1,21 +1,39 @@
 require 'spec_helper'
+require 'pry'
 
+# MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_URI']}}, 'production')
+# MongoMapper.database = ENV['MONGODB_DB'] || "boogle"
+#
+# class Page
+#   include MongoMapper::Document
+#
+#   key :pageId
+#   key :content
+# end
 
+class ScoreMachine
+  class << self
+    def calculate(content, query)
+      content_words = content.split(' ')
+      query_words = query.split(' ')
+
+      (query_words & content_words).count
+    end
+  end
+end
 
 describe "Boogle" do
-  context "Data" do
-    it "should insert new index" do
-
+  context "Score" do
+    it "should score page search" do
+      page_content = "the quick brown fox jumped over the lazy dog"
+      query = 'quick but lazy brown dog'
+      expect(ScoreMachine.calculate page_content, query).to eq 4
     end
 
-    it "should score page by query"
-
-    context "Score" do
-      it "score should ignore case"
-      it "score should ignore punctuation"
-      it "score should ignore word order"
-      it "score should ignore frequency"
-    end
+    it "score should ignore case"
+    it "score should ignore punctuation"
+    it "score should ignore word order"
+    it "score should ignore frequency"
   end
 end
 
