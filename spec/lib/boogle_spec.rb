aafc3409e2d256dc5f0503254ed1d16a82fca2a6
renatodex <renatodex@gmail.com>
2014-10-11 16:14:49 -0300

    Adding some pending specs

--- spec/lib/boogle_spec.rb
+++ spec/lib/boogle_spec.rb
@@ -1,7 +1,20 @@
 require 'spec_helper'
 
 describe "Boogle" do
-  it "should pass" do
-    expect(true).to eq true
+  context "API" do
+    context "POST /index"
+    context "GET /search"
+  end
+
+  context "Data" do
+    it "should insert new index"
+    it "should score page by query"
+
+    context "Score" do
+      it "score should ignore case"
+      it "score should ignore punctuation"
+      it "score should ignore word order"
+      it "score should ignore frequency"
+    end
   end
 end
